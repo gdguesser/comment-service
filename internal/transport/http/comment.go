ec82764d2ece27e07f60e923a8c055705c2b4d93
Gabriel Guesser <gdguesser@gmail.com>
2022-10-01 17:53:26 +0100

    Implementing DELETE and UPDATE handlers

--- internal/transport/http/comment.go
+++ internal/transport/http/comment.go
@@ -17,6 +17,10 @@ type CommentService interface {
 	DeleteComment(ctx context.Context, ID string) error
 }
 
+type Response struct {
+	Message string
+}
+
 func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
 	var cmt comment.Comment
 	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
@@ -55,9 +59,47 @@ func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) UpdateComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	var cmt comment.Comment
+	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
+		return
+	}
+
+	cmt, err := h.Service.UpdateComment(r.Context(), id, cmt)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(cmt); err != nil {
+		panic(err)
+	}
 }
 
 func (h *Handler) DeleteComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := h.Service.DeleteComment(r.Context(), id)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(Response{Message: "Successfully deleted"}); err != nil {
+		panic(err)
+	}
 }
