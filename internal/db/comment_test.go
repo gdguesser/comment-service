8e0ab13c0deffa2d89bad3fbffda9f557bce479e
Gabriel Guesser <gdguesser@gmail.com>
2022-10-02 23:12:00 +0100

    Testing delete method on db layer

--- internal/db/comment_test.go
+++ internal/db/comment_test.go
@@ -4,7 +4,6 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/gdguesser/comment-service/internal/comment"
@@ -26,7 +25,22 @@ func TestCommentDatabase(t *testing.T) {
 		newCmt, err := db.GetComment(context.Background(), cmt.ID)
 		assert.NoError(t, err)
 		assert.Equal(t, "slug", newCmt.Slug)
+	})
 
-		fmt.Println("testing the creation of comments")
+	t.Run("test delete comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "new-slug",
+			Author: "gabriel",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		err = db.DeleteComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+
+		_, err = db.GetComment(context.Background(), cmt.ID)
+		assert.Error(t, err)
 	})
 }
