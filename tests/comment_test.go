98d09a8bb74040f9d1941272b8bae7b73e3cf9bb
Gabriel Guesser <gdguesser@gmail.com>
2022-10-05 14:24:47 +0100

    Creating a func to generate token for the tests instead of using a fixed token

--- tests/comment_test.go
+++ tests/comment_test.go
@@ -1,18 +1,30 @@
-//:gobuild e2e
+// :gobuild e2e
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-resty/resty/v2"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 )
+
+func createToken() string {
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, err := token.SignedString([]byte("missionimpossible"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return tokenString
+}
 
 func TestPostComment(t *testing.T) {
 	t.Run("can post comment", func(t *testing.T) {
 		client := resty.New()
 		resp, err := client.R().
-			SetHeader("Authorization", "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.2yPNTTY3Y5jUwYBPAJAfUc84Ybv2qPbZY_OHI7tzuug").
+			SetHeader("Authorization", "bearer " + createToken()).
 			SetBody(`{"slug": "/", "author": "Gabriel", "body": "hello peeps"}`).
 			Post("http://localhost:8080/api/v1/comment")
 
