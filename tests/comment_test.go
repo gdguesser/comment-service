1c2d8f312bfed753d44310f95bcd2bfd2444cf99
Gabriel Guesser <gdguesser@gmail.com>
2022-10-05 15:29:31 +0100

    Fixing build tag on e2e tests and adding more tests to it

--- tests/comment_test.go
+++ tests/comment_test.go
@@ -1,4 +1,5 @@
-// :gobuild e2e
+//go:build e2e
+
 package tests
 
 import (
@@ -24,7 +25,7 @@ func TestPostComment(t *testing.T) {
 	t.Run("can post comment", func(t *testing.T) {
 		client := resty.New()
 		resp, err := client.R().
-			SetHeader("Authorization", "bearer " + createToken()).
+			SetHeader("Authorization", "bearer "+createToken()).
 			SetBody(`{"slug": "/", "author": "Gabriel", "body": "hello peeps"}`).
 			Post("http://localhost:8080/api/v1/comment")
 
@@ -50,5 +51,48 @@ func TestGetComment(t *testing.T) {
 		resp, err := client.R().Get("http://localhost:8080/api/v1/comment/01GEM0SMER7AK27A1NED7R17V6")
 		assert.NoError(t, err)
 		assert.Equal(t, 200, resp.StatusCode())
+	})
+}
+
+func TestUpdateComment(t *testing.T) {
+	t.Run("can update comment", func(t *testing.T) {
+		client := resty.New()
+		resp, err := client.R().
+			SetHeader("Authorization", "bearer "+createToken()).
+			SetBody(`{"slug": "/", "author": "Gabriel", "body": "hello peeps, update test"}`).
+			Put("http://localhost:8080/api/v1/comment/01GEA77B34V4Z7M0KWN10Z1PE6")
+
+		assert.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode())
+	})
+
+	t.Run("cannot update comment without JWT", func(t *testing.T) {
+		client := resty.New()
+		resp, err := client.R().
+			SetBody(`{"slug": "/", "author": "Gabriel", "body": "hello peeps, update test"}`).
+			Put("http://localhost:8080/api/v1/comment/01GEA77B34V4Z7M0KWN10Z1PE6")
+
+		assert.NoError(t, err)
+		assert.Equal(t, 401, resp.StatusCode())
+	})
+}
+
+func TestDeleteComment(t *testing.T) {
+	t.Run("can delete comment", func(t *testing.T) {
+		client := resty.New()
+
+		resp, err := client.R().
+			SetHeader("Authorization", "bearer "+createToken()).
+			Delete("http://localhost:8080/api/v1/comment/01GEM7MT29W34WDQE0FT1PRP5H")
+		assert.NoError(t, err)
+		assert.Equal(t, 200, resp.StatusCode())
+	})
+
+	t.Run("cannot delete comment without JWT", func(t *testing.T) {
+		client := resty.New()
+
+		resp, err := client.R().Delete("http://localhost:8080/api/v1/comment/01GEM7MT29W34WDQE0FT1PRP5H")
+		assert.NoError(t, err)
+		assert.Equal(t, 401, resp.StatusCode())
 	})
 }
